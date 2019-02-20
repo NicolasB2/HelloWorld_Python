fd8da7e6325110b31977354511320aaae782a78f
NicolasBiojo <nikobiober@gmail.com>
2019-02-19 19:17:09 -0500

    Add hellow world project

---
+++ hello_World.py
@@ -0,0 +1,14 @@
+# Print by console string values
+x = 1
+if x == 1:
+    print ("hello")
+
+mystring = 'hello'
+print(mystring)
+mystring = "hello"
+print(mystring)
+
+varx = 7
+
+print(varx)
+print (5+5)
