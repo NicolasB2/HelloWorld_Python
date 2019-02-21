041ad39b90d41fac06acaa606f7fc2d401c27799
NicolasBiojo <nikobiober@gmail.com>
2019-02-21 01:27:58 -0500

    Add project about lists

---
+++ lists.py
@@ -0,0 +1,32 @@
+#first example
+my_list = []
+my_list.append(1)
+my_list.append(2)
+my_list.append(3)
+print(my_list[0])
+print(my_list[1])
+print(my_list[2])
+print(" ")
+
+#second example
+my_list = [4,5,6]
+for x in my_list:
+    print(x)
+print(" ");
+
+
+#third example
+numbers = []
+strings = []
+names = ["Nicolas", "Daniela", "Sara", "David"]
+
+numbers.append(7)
+numbers.append(8)
+numbers.append(9)
+
+strings.append("hello")
+strings.append("world")
+
+print(numbers)
+print(strings)
+print(names);
