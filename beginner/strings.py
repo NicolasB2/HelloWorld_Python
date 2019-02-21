463408597ab2eef7c56cc7f2e13678de2e985a8c
NicolasBiojo <nikobiober@gmail.com>
2019-02-21 09:12:22 -0500

    Add example of write a format string

--- beginner/strings.py
+++ beginner/strings.py
@@ -1,11 +1,33 @@
-#to declarate variables type String
+#concatenate strings
 hello = "hello"
 world = "world"
 helloworld = hello + " " + world
 print(helloworld)
+print("")
 
+#multiplicated strings
 hellos = "hello" * 10
 print(hellos)
+print("")
 
+#formate like this: "Hello, John!"
+name = "John"
+print("Hello, %s!" % name)
+print("")
 
+# formate like this: "John is 23 years old."
+name = "John"
+age = 23
+print("%s is %d years old." % (name, age))
+print("")
 
+#formate like this: "A list: [1, 2, 3]"
+mylist = [1,2,3]
+print("A list: %s" % mylist)
+print("")
+
+#formate like this: "Hello John doe. Your current balance is 53.44."
+data = ("John", "Doe", 53.44)
+format_string = "Hello %s %s. Your current balance is $%s."
+print(format_string % data)
+print("")
