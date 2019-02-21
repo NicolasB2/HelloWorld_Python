41729ba8eb2fded4d1af1daaab9594a5178217da
NicolasBiojo <nikobiober@gmail.com>
2019-02-21 12:47:08 -0500

    Add loops examples

---
+++ beginner/loops.py
@@ -0,0 +1,25 @@
+# Prints out 2 3 5 7
+primes = [2, 3, 5, 7]
+for prime in primes:
+    print(prime)
+
+# Prints out the numbers 0,1,2,3,4
+for x in range(5):
+    print(x)
+
+# Prints out 3,4,5
+for x in range(3, 6):
+    print(x)
+
+# Prints out only odd numbers - 1,3,5,7,9
+for x in range(10):
+    # Check if x is even
+    if x % 2 == 0:
+        continue
+    print(x)
+
+#Prints out the numbers 0,1,2,3,4
+count = 0
+while(count<5):
+    print(count)
+    count +=1
