e7802d795f09aadc2984f3607a4be6ee2d0b7694
NicolasBiojo <nikobiober@gmail.com>
2019-02-22 14:24:32 -0500

    Add example of yield generator and random

--- advanced/generators.py
+++ advanced/generators.py
@@ -8,3 +8,17 @@ def contador(max):
 mycont = contador(5)
 for i in mycont:
     print(i)
+
+
+# example of lotery with yield generator and random
+import random
+def lottery():
+    # returns 6 numbers between 1 and 40
+    for i in range(6):
+        yield int(random.uniform(1,40))
+
+    # returns a 7th number between 1 and 15
+    yield int(random.uniform(1, 15))
+
+for j in lottery():
+       print(j)
