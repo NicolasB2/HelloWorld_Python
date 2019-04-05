de216cd03ca86532258b723a1d40b21be5ddd60b
NicolasBiojo <nikobiober@gmail.com>
2019-04-05 08:02:41 -0500

    finish read file example

---
+++ 3.Advanced/file_read.py
@@ -0,0 +1,20 @@
+#open file for reading : f = open("file.txt", "r")
+
+#'Hel'
+f = open('../sources/read.txt')
+dato1 = f.read(3)
+print(dato1)
+f.close()
+
+#'Hello\n'
+g = open('../sources/read.txt')
+dato2 = g.readline()
+print(dato2)
+g.close()
+
+
+#'Hello\nWord\n'
+h = open('../sources/read.txt')
+dato3 = h.read()
+print (dato3)
+h.close()
