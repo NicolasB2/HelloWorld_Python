b4b72a54c8475092d0370595cb961bfc883bd87e
NicolasBiojo <nikobiober@gmail.com>
2019-04-05 15:41:08 -0500

    Add serialization example

--- 3.Advanced/serialization.py
+++ 3.Advanced/serialization.py
@@ -1,8 +1,26 @@
-# example with json
 import json
-json_string = json.dumps([1, 2, 3, "a", "b", "c"])
-print(json_string)
-"""print ("-----")"""
 import pickle
-pickled_string = pickle.dumps([1, 2, 3, "a", "b", "c"])
-print(pickle.loads(pickled_string))
+
+#JSON is human-readable
+json_string = json.dumps([1, 2, 3])
+g = open("../sources/json.txt","w")
+g.write(json_string)
+g.close()
+
+x = json.loads(json_string)
+x.append(4)
+x.append(5)
+x.append(6)
+print x
+
+#PICKLED is human-unredable
+pickle_string = pickle.dumps(["a", "b", "c"])
+f = open("../sources/pickle.txt","w")
+f.write(pickle_string)
+f.close()
+
+y = pickle.loads(pickle_string)
+y.append('d')
+y.append('e')
+y.append('f')
+print y
